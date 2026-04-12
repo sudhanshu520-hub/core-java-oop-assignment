e2b627f5d4cf5cd18f3d4e8a7742c4eecd0877c2
Sudhanshu Pandey <sudh2005anshu@gmail.com>
2026-04-12 23:02:36 +0530

    Add files via upload

---
+++ StudentNameDemo.java
@@ -0,0 +1,22 @@
+public class StudentNameDemo {
+    
+    public static void main(String[] args) {
+
+        // Student name
+        String name = "Sudhanshu";
+
+        // Original name
+        System.out.println("Original Name: " + name);
+
+        // Uppercase
+        System.out.println("Uppercase: " + name.toUpperCase());
+
+        // Lowercase
+        System.out.println("Lowercase: " + name.toLowerCase());
+
+        // Length of name
+        System.out.println("Length of Name: " + name.length());
+    }
+}
+    
+
