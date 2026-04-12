5f47bdc08c4524fb06c06ecd75817d2ebb1420d8
Sudhanshu Pandey <sudh2005anshu@gmail.com>
2026-04-12 23:05:28 +0530

    Add files via upload
    Create an array of 5 marks and print them using loop

---
+++ Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args){
+        int[ ] number = { 5,3,6,6,4,};
+    
+    
+
+       System.out.println("Array content");
+        for(int i=0; i<=number.length; i++){
+            System.out.println(number[i] +"");
+        }
+    }
+}
